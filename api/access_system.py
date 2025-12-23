f96ec992e312627b7fd04e684018bc5c91689a70
iwanttobelievee <artemkursaevkz@gmail.com>
2025-12-23 19:00:15 +0500

    fix: update API URL to use HTTPS and add certificate verification

--- api/access_system.py
+++ api/access_system.py
@@ -1,7 +1,7 @@
 import requests
 import numpy as np
 
-url = "http://localhost:8081/api/v1/embedding/validate"
+url = "https://localhost/api/v1/embedding/validate"
 
 def validate_embedding(embedding: np.ndarray):
     if embedding is None:
@@ -12,7 +12,7 @@ def validate_embedding(embedding: np.ndarray):
     data = {
         "vector": embedding_list
     }
-    response = requests.post(url, json=data)
+    response = requests.post(url, json=data, verify=False, cert=('./certs/client.crt', './certs/client.key'))
 
     if response.status_code == 200:
         return True, response.json()
